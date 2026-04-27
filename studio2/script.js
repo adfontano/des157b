f271a2fb6dc22633e99f0d7073e7b3638132a00d
adfontano <aidanxf44@gmail.com>
2026-04-26 17:30:21 -0700

    updating check yourself

--- studio2/script.js
+++ studio2/script.js
@@ -2,27 +2,61 @@
     'use strict';
     console.log('reading js');
 
-    const blocks = document.querySelector('.block');
-    const planner = document.querySelector('.planner');
-    //const plannerText = document.querySelector('.planner-text');
+    const block = document.querySelector('.block');
+    const myDay = document.querySelector('#my-day');   
+    let activeBlock = null;
+
 
     async function getData(){
         const myDay = await fetch('data/day.json');
         const data = await myDay.json();
         console.log(data);
-        makeBlocks(data);
+        makeBlocks(data);  
     } 
+
     function makeBlocks(data){
         const numBlocks = data.schedule;
 
         numBlocks.forEach(function(data){
-            const newBlock = blocks.cloneNode(true);
+            const newBlock = block.cloneNode(true);
+            
+
             planner.appendChild(newBlock);
-            newBlock.textContent = data.time; 
-        });
-        // need to make each block's height relative to the amount of hours it represents
+            newBlock.innerHTML = `<p>${data.time}<p>`; 
+    
+            newBlock.style.flex = data.hours;
+            console.log(data.hours);
+
+            newBlock.addEventListener('click',function(){
+                myDay.innerHTML = `<img src='${data.images}'>
+                <div>
+                    <p>Time Started: ${data.time}</p>
+                    <p>Activity: ${data.activity}</p>
+                    <p>Feeling: ${data.feeling}</p>
+                </div>`;
+                blockColor(); 
+                
+            })
+        });   
+        
+    }
+
+    function blockColor(){
+        const allBlocks = document.querySelectorAll('.block');
+        for(const oneBlock of allBlocks){
+                oneBlock.addEventListener('click', function(event){  
+                    event.currentTarget.style.backgroundColor = '#f7e2a5';
+                    
+                    if(activeBlock !== null && activeBlock !== event.currentTarget){
+                        activeBlock.style.backgroundColor = '#f7efcb';
+                    }
+
+                    activeBlock = event.currentTarget;
+                });
+        };
     }
 
     getData();
+    
     
 })();
